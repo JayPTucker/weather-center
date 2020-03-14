67605252f3a51b634ab6a76bb6a7fbcdffecd346
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-03-13 21:22:42 -0500

    Youre now able to type in any city.  The Results are logged into the console and the city (if its valid) will print on the screen.  Clicking it does nothing yet

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,27 @@ $("#search-btn").on('click', function(event) {
     var searchInput = $("#city-search").val()
     console.log(searchInput);
 
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q="
-    + searchInput + "&appid=e8bec4199b95f357f589f6ba4bdcd7c3";
+    
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(queryURL);
-        console.log(response);
-    });
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=e8bec4199b95f357f589f6ba4bdcd7c3";
+
+        $.ajax({
+            url: queryURL,
+            success: function() {
+                $("#search-history").append("<li><input id='history-search-btn' class='previous-search' type='submit' value='" + searchInput + "'></li>");
+            },
+            method: "GET",
+            error: function() {
+                alert('City Not Found.  Try Again.')
+            } 
+        }).then(function(response) {
+            console.log(response);
+        });
+
+    
+    // THE HISTORY BUTTONS
+    $("#history-search-btn").on('click', function(event) {
+        event.preventDefault();
+        console.log($(this).val());
+    })
 });
