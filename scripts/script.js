6c74dc3d3fcc1b3d460ce602a80369e2c8f65f96
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-03-13 20:52:21 -0500

    Made some detail changes in the HTML Code

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
-var searchBtn = $("#search-btn")
-var searchInput = $("#city-search")
-
-searchBtn.on('click', function(event) {
+$("#search-btn").on('click', function(event) {
     event.preventDefault();
-    console.log(searchInput)
-})
+    // THE INPUT THAT'S TYPED INTO THE BOX (HENCE THE .VAL)
+    var searchInput = $("#city-search").val()
+    console.log(searchInput);
+
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q="
+    + searchInput + "&appid=e8bec4199b95f357f589f6ba4bdcd7c3";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+    });
+});
