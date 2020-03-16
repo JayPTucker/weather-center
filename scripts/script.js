b222e3d230b2d24a18ba193973150b24708ca99d
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-03-16 00:47:23 -0500

    Created a Working and Fully Functional 5 Day Forecast.  Every New Search deletes what was there previously, so the page isn't constantly trying to load 5 Day Forecasts when you might not need them.

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,8 @@ $("#search-btn").on('click', function(event) {
 
         // IF THE CITY IS FOUND DO THIS
         success: function() {
+        // Clears the 5-Day-Forecast Chart
+        $(".5DFresults").text("");
         // Checks if the value is already in the list, if it is alert that the city is already there.
         if(searchInput === (($("#" + searchInput).val()))) {
             alert("This city already exists and is in your History tab.")
@@ -77,7 +79,6 @@ $("#search-btn").on('click', function(event) {
 
         }).then(function(response) {
 
-            console.log(response)
             var uvIndex = response.value;
             var printUVI = $("<p id='uvIndex'>").text("UV Index: " + uvIndex);
             
@@ -97,35 +98,124 @@ $("#search-btn").on('click', function(event) {
         $("#weatherDiv").append(weatherDiv);
 
         // -------------------------------------------------------------
-        // 5 DAY FORECAST:
+        // 5 DAY FORECAST CREATION:
 
-        var FiveDFDiv = $("#5DFDiv");
+        var FDFbtnDiv = $("#5DFbtnDiv");
         var print5DFbtn = ($("<button id='5DFbtn'>").text("View 5 Day Forecast"));
+        var printCurrentCity = ($("<p>").text("Current City:" + searchInput))
         // Deletes the Button everytime so it doesn't duplicate itself
-        FiveDFDiv.text("");
+        FDFbtnDiv.text("");
 
-        FiveDFDiv.append(print5DFbtn);
+        FDFbtnDiv.append(print5DFbtn);
+        FDFbtnDiv.append(printCurrentCity);
 
         $("#5DFbtn").on('click', function() {
-            
             var FiveDayqueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=e8bec4199b95f357f589f6ba4bdcd7c3"
 
             $.ajax({
                 url: FiveDayqueryURL,
-                method: "GET"
+                method: "GET",
+                success: function() {
+                    $(".5DFresults").text("");
+                }
             }).then(function(response) {
-                console.log(response.list);
                 console.log("-----------------")
-                // DAY 0
-                console.log(response.list[0])
+
                 // DAY 1
-                console.log(response.list[9])
+                console.log(response.list[0])
+                var day1Date = ((response.list[0].dt_txt).substring(0,10));
+                var day1IconID = response.list[0].weather[0].icon
+                var day1Icon = "http://openweathermap.org/img/w/" + day1IconID + ".png";
+                var day1Temp = (((response.list[0].main.temp) * (9/5) - 459.67).toFixed(2));
+                var day1Humidity = response.list[0].main.humidity;
+
+                var printDay1Date = ($("<p>").text(day1Date));
+                var printDay1Icon = ($("<img>").attr("src", day1Icon));
+                var printDay1Temp = ($("<p>").text("Temperature: " + day1Temp));
+                var printDay1Humidity = ($("<p>").text("Humidity: " + day1Humidity));
+
+                
+                ($("#day1Div")).append(printDay1Date);
+                ($("#day1Div")).append(printDay1Icon);
+                ($("#day1Div")).append(printDay1Temp);
+                ($("#day1Div")).append(printDay1Humidity);
+                
                 // DAY 2
-                console.log(response.list[18])
+                console.log(response.list[8])
+                var day2Date = ((response.list[8].dt_txt).substring(0,10));
+                var day2IconID = response.list[8].weather[0].icon
+                var day2Icon = "http://openweathermap.org/img/w/" + day2IconID + ".png";
+                var day2Temp = (((response.list[8].main.temp) * (9/5) - 459.67).toFixed(2));
+                var day2Humidity = response.list[8].main.humidity;
+
+                var printDay2Date = ($("<p>").text(day2Date));
+                var printDay2Icon = ($("<img>").attr("src", day2Icon));
+                var printDay2Temp = ($("<p>").text("Temperature: " + day2Temp));
+                var printDay2Humidity = ($("<p>").text("Humidity: " + day2Humidity));
+
+                
+                ($("#day2Div")).append(printDay2Date);
+                ($("#day2Div")).append(printDay2Icon);
+                ($("#day2Div")).append(printDay2Temp);
+                ($("#day2Div")).append(printDay2Humidity);
+
                 // DAY 3
-                console.log(response.list[28])
+                console.log(response.list[16])
+                var day3Date = ((response.list[16].dt_txt).substring(0,10));
+                var day3IconID = response.list[16].weather[0].icon
+                var day3Icon = "http://openweathermap.org/img/w/" + day3IconID + ".png";
+                var day3Temp = (((response.list[16].main.temp) * (9/5) - 459.67).toFixed(2));
+                var day3Humidity = response.list[16].main.humidity;
+
+                var printDay3Date = ($("<p>").text(day3Date));
+                var printDay3Icon = ($("<img>").attr("src", day3Icon));
+                var printDay3Temp = ($("<p>").text("Temperature: " + day3Temp));
+                var printDay3Humidity = ($("<p>").text("Humidity: " + day3Humidity));
+
+                
+                ($("#day3Div")).append(printDay3Date);
+                ($("#day3Div")).append(printDay3Icon);
+                ($("#day3Div")).append(printDay3Temp);
+                ($("#day3Div")).append(printDay3Humidity);
+
                 // DAY 4
-                console.log(response.list[39])
+                console.log(response.list[24])
+                var day4Date = ((response.list[24].dt_txt).substring(0,10));
+                var day4IconID = response.list[24].weather[0].icon
+                var day4Icon = "http://openweathermap.org/img/w/" + day4IconID + ".png";
+                var day4Temp = (((response.list[24].main.temp) * (9/5) - 459.67).toFixed(2));
+                var day4Humidity = response.list[24].main.humidity;
+
+                var printDay4Date = ($("<p>").text(day4Date));
+                var printDay4Icon = ($("<img>").attr("src", day4Icon));
+                var printDay4Temp = ($("<p>").text("Temperature: " + day4Temp));
+                var printDay4Humidity = ($("<p>").text("Humidity: " + day4Humidity));
+
+                
+                ($("#day4Div")).append(printDay4Date);
+                ($("#day4Div")).append(printDay4Icon);
+                ($("#day4Div")).append(printDay4Temp);
+                ($("#day4Div")).append(printDay4Humidity);
+
+                // DAY 5
+                console.log(response.list[32])
+                var day5Date = ((response.list[32].dt_txt).substring(0,10));
+                var day5IconID = response.list[32].weather[0].icon
+                var day5Icon = "http://openweathermap.org/img/w/" + day5IconID + ".png";
+                var day5Temp = (((response.list[32].main.temp) * (9/5) - 459.67).toFixed(2));
+                var day5Humidity = response.list[32].main.humidity;
+
+                var printDay5Date = ($("<p>").text(day5Date));
+                var printDay5Icon = ($("<img>").attr("src", day5Icon));
+                var printDay5Temp = ($("<p>").text("Temperature: " + day5Temp));
+                var printDay5Humidity = ($("<p>").text("Humidity: " + day5Humidity));
+
+                
+                ($("#day5Div")).append(printDay5Date);
+                ($("#day5Div")).append(printDay5Icon);
+                ($("#day5Div")).append(printDay5Temp);
+                ($("#day5Div")).append(printDay5Humidity);
+
             });
         });
     });
